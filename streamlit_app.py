99fce42d0f0b7d3205a53d9bf359bc441660e928
Magnait_Marv <149040436+LITE2023GHUB@users.noreply.github.com>
2025-05-21 01:11:12 +0800

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import numpy as np
+import os
+import urllib.request
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from PIL import Image
+
+MODEL_URL = "https://drive.google.com/uc?id=YOUR_FILE_ID"
+MODEL_PATH = "weather_model.h5"
+
+@st.cache_resource
+def download_and_load_model():
+    if not os.path.exists(MODEL_PATH):
+        with st.spinner("Downloading model..."):
+            urllib.request.urlretrieve(MODEL_URL, MODEL_PATH)
+    model = load_model(MODEL_PATH)
+    return model
+
+model = download_and_load_model()
